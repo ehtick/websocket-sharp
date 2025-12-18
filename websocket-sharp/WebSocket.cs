2c07dcf1fdb5231c498dcfc19a23f5bcca75b70f
sta <sta.blockhead@gmail.com>
2025-12-18 16:21:56 +0900

    [Modify] Edit it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -4030,8 +4030,8 @@ namespace WebSocketSharp
     /// <paramref name="data"/> is <see langword="null"/>.
     /// </exception>
     /// <exception cref="InvalidOperationException">
-    /// The SendAsync method is not available when the current state of
-    /// the interface is not Open.
+    /// This method is not available when the current state of the interface
+    /// is not Open.
     /// </exception>
     public void SendAsync (string data, Action<bool> completed)
     {
